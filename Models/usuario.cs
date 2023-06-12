22a9f28949a0ac7c2857534c828f04f2b94faa87
qualquer <qualquer>
2023-06-12 09:07:19 -0300

    primeiro commit

---
+++ Models/usuario.cs
@@ -0,0 +1,12 @@
+namespace Cadastro.Models
+{
+    public class usuario
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public int Cpf { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Preferencia { get; set; }
+    }
+}
